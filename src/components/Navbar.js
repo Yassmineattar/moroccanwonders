5639d28b32264be5a687ad3aa9aa0dd5688908f2
Jijiii123 <jihaneameziane2004@gmail.com>
2024-12-01 22:37:56 +0100

    navbar modifs

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FaUser, FaHeart } from "react-icons/fa";
+import { IoLanguageOutline } from "react-icons/io5";
 import "./Navbar.css";
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav className="navbar navbar-expand-lg navbar-dark">
       <div className="container">
+        {/* Brand Name */}
         <Link className="navbar-brand" to="/">
           MoroccoWonders
         </Link>
+
+        {/* Toggler for mobile view */}
         <button
           className="navbar-toggler"
           type="button"
@@ -17,7 +22,9 @@ function Navbar() {
         >
           <span className="navbar-toggler-icon"></span>
         </button>
+
         <div className="collapse navbar-collapse" id="navbarNav">
+          {/* Navigation Links */}
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
               <Link className="nav-link" to="/destinations">
@@ -34,9 +41,31 @@ function Navbar() {
                 Traditional Store
               </Link>
             </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/plan">
+            <li className="nav-item dropdown">
+              <Link className="nav-link dropdown-toggle" to="/plan" role="button" data-bs-toggle="dropdown">
                 Plan Your Trip
+              </Link>
+              <ul className="dropdown-menu">
+                <li><Link className="dropdown-item" to="/plan/itinerary">Itinerary</Link></li>
+                <li><Link className="dropdown-item" to="/plan/hotels">Hotels</Link></li>
+              </ul>
+            </li>
+          </ul>
+          {/* Icons Section */}
+          <ul className="navbar-nav align-items-center">
+            <li className="nav-item">
+              <Link className="nav-link" to="/favorites">
+                <FaHeart className="icon" />
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/profile">
+                <FaUser className="icon" />
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/language">
+                <IoLanguageOutline className="icon" /> EN
               </Link>
             </li>
           </ul>
