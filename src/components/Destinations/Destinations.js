4e82f754013935f6e03a423a0fbe4e975de0cb45
greeen11 <elkhdarayman5@gmail.com>
2024-12-08 04:32:38 +0100

    routing and categories (khasna nrecuperiwhon mn api bach ytl3o hhhh)

---
+++ src/components/Destinations/Destinations.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Destinations = () => {
+  return <h1>Explore Morocco's Best Destinations</h1>;
+};
+
+export default Destinations;
---
+++ src/components/landing_page/Home.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Carousel from "./Carousel";
+import WhyVisitMorocco from "./WhyVisitMorocco";
+import BiggestEvent from "./BiggestEvent";
+import DestinationSection from './DestinationSection';
+
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <Carousel />
+      <WhyVisitMorocco />
+      <BiggestEvent />
+      <DestinationSection />
+
+      
+    </div>
+
+
+  );
+};
+
+export default Home;
