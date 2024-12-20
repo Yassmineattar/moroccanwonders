aa24675fe243b2b0a2f6aec70705fe0078734bd7
Jijiii123 <jihaneameziane2004@gmail.com>
2024-12-20 21:04:16 +0100

    changement du caroussel

--- src/components/landing_page/Carousel.js
+++ src/components/landing_page/Carousel.js
@@ -1,83 +1,120 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Carousel.css";
 
 function Carousel() {
-  return (
-    <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
-      <div className="carousel-indicators">
-        <button
-          type="button"
-          data-bs-target="#carouselExampleIndicators"
-          data-bs-slide-to="0"
-          className="active"
-          aria-current="true"
-        ></button>
-        <button
-          type="button"
-          data-bs-target="#carouselExampleIndicators"
-          data-bs-slide-to="1"
-        ></button>
-        <button
-          type="button"
-          data-bs-target="#carouselExampleIndicators"
-          data-bs-slide-to="2"
-        ></button>
-      </div>
-      <div className="carousel-inner">
-      <div className="carousel-item active">
-  <img
-    src="/images/chefchaouen.jpg"
-    className="d-block w-100"
-    alt="Chefchaouen"
-  />
-  <div className="carousel-caption">
-    <h5>Explore Morocco's Blue Gem</h5>
-    <p>
-    Chefchaouen is undoubtedly one of Morocco's (and possibly the world's) most
-    Instagrammable spots, thanks to the blue hues that surround every nook and cranny.
-  </p>
-    </div>
-</div>
+  const [currentSlide, setCurrentSlide] = useState(0); // Suivre le slide actuel
 
-        <div className="carousel-item">
-          <img
-            src="/images/sahara.jpg" 
-            className="d-block w-100"
-            alt="Deserts"
-          />
-          <div className="carousel-caption d-none d-md-block">
-            <h5>Discover the Sahara</h5>
-            <p>Experience the vastness of Morocco's deserts.</p>
-          </div>
-        </div>
-        <div className="carousel-item">
-          <img
-            src="/images/fna.jpeg" 
-            className="d-block w-100"
-            alt="Culture"
-          />
-          <div className="carousel-caption d-none d-md-block">
-            <h5>Immerse in Moroccan Culture</h5>
-            <p>From music to cuisine, Morocco has it all.</p>
-          </div>
-        </div>
+  // Fonction de gestion du changement de slide
+  const handleSlideChange = (index) => {
+    setCurrentSlide(index);
+  };
+
+  return (
+    <div>
+      {/* Barre de recherche */}
+      <div className="search-bar">
+      <input type="text" placeholder="Search for city, place, product, event" />
+      <i class="fas fa-search"></i> 
       </div>
-      <button
-        className="carousel-control-prev"
-        type="button"
-        data-bs-target="#carouselExampleIndicators"
-        data-bs-slide="prev"
+
+      <div
+        id="carouselExampleIndicators"
+        className="carousel slide"
+        data-bs-ride="carousel"
+        onSlide={(e) => handleSlideChange(e.to)} // Détecter le changement de slide
       >
-        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-      </button>
-      <button
-        className="carousel-control-next"
-        type="button"
-        data-bs-target="#carouselExampleIndicators"
-        data-bs-slide="next"
-      >
-        <span className="carousel-control-next-icon" aria-hidden="true"></span>
-      </button>
+        <div className="carousel-indicators">
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="0"
+            className={currentSlide === 0 ? "active" : ""}
+            aria-current="true"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="1"
+            className={currentSlide === 1 ? "active" : ""}
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="2"
+            className={currentSlide === 2 ? "active" : ""}
+          ></button>
+        </div>
+
+        <div className="carousel-inner">
+          <div className={`carousel-item ${currentSlide === 0 ? "active" : ""}`}>
+          <img
+              src="/images/fna.jpeg"
+              className="d-block w-100"
+              alt="Culture"
+            />
+            <div className="carousel-caption">
+              <h5>Immerse in Moroccan Culture</h5>
+              <p>From music to cuisine, Morocco has it all.</p>
+              <div className="carousel-buttons">
+                <button className={`btn-slide-${currentSlide}`}>Explore the Culture</button>
+                <button className={`btn-slide-${currentSlide}`}>Discover the Traditions</button>
+              </div>
+            </div>
+          </div>
+
+          <div className={`carousel-item ${currentSlide === 1 ? "active" : ""}`}>
+            <img
+              src="/images/sahara.jpg"
+              className="d-block w-100"
+              alt="Deserts"
+            />
+            <div className="carousel-caption">
+              <h5>Discover the Sahara</h5>
+              <p>Experience the vastness of Morocco's deserts.</p>
+              <div className="carousel-buttons">
+                <button className={`btn-slide-${currentSlide}`}>Explore the Desert</button>
+                <button className={`btn-slide-${currentSlide}`}>Book a Tour</button>
+              </div>
+            </div>
+          </div>
+          
+          <div className={`carousel-item ${currentSlide === 2 ? "active" : ""}`}>
+          <img
+              src="/images/chefchaouen.jpg"
+              className="d-block w-100"
+              alt="Chefchaouen"
+            />
+            <div className="carousel-caption">
+              <h5>Explore Morocco's Blue Gem</h5>
+              <p>
+                Chefchaouen is undoubtedly one of Morocco's (and possibly the world's) most
+                Instagrammable spots, thanks to the blue hues that surround every nook and cranny.
+              </p>
+              <div className="carousel-buttons">
+                <button className={`btn-slide-${currentSlide}`}>Explore our Culture</button>
+                <button className={`btn-slide-${currentSlide}`}>Explore our Destinations</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide="prev"
+        >
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide="next"
+        >
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        </button>
+      </div>
     </div>
   );
 }
